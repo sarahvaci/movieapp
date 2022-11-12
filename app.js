dbc0238eec68a30794648467b77a40b399575ec7
SarahVee <sarahvacio@gmail.com>
2022-11-11 21:32:39 -0500

    install pkg

--- app.js
+++ app.js
@@ -2,4 +2,15 @@ const express = require('express');
 const app = express();
 
 app.set('views', path.join(_dirname, 'views'));
+app.set('view engine', 'ejs');
 
+app.use(express.static('public'));
+
+//create first route
+app.get('/search', (req,res)=> {
+    res.render('search');
+});
+
+app.listen(3000,()=>{
+    console.log('server started at port 3000.');
+});
