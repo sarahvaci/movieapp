2066be0dfb8b1fae1a93ec11f6a35c4d54265e47
SarahVee <sarahvacio@gmail.com>
2022-11-11 21:36:16 -0500

    simple fix dir

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const path = require ('path');
 
-app.set('views', path.join(_dirname, 'views'));
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(express.static('public'));
