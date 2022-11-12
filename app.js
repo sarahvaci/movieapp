5c3aac699574dc19446541fa3506843ac987a29f
SarahVee <sarahvacio@gmail.com>
2022-11-11 22:07:21 -0500

    center content

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express'); 
 const app = express();
-const path = require ('path');
+const path = require('path');
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -15,3 +15,6 @@ app.get('/search', (req,res)=> {
 app.listen(3000,()=>{
     console.log('server started at port 3000.');
 });
+//http://localhost:3000
+
+
